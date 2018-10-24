2ead577aa9aa9cc2054a8620b174ee5d7a586845
Satya Prakash <35588980+satya7289@users.noreply.github.com>
2018-10-24 18:00:31 +0530

    Update urls.py

--- DDA/DDA/urls.py
+++ DDA/DDA/urls.py
@@ -9,3 +9,9 @@ urlpatterns = [
     path('', include('app.urls')),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
